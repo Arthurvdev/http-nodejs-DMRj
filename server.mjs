48737c5037c9b9c9f4013296b937fd7081a8200e
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-21 14:20:40 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,7 +1,11 @@
 import { createServer } from 'http';
 import https from 'https';
+import url from 'url';
 
 createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  const playerName = queryObject.name;
+  
   const options = {
     method: 'GET',
     headers: {
@@ -9,7 +13,7 @@ createServer((req, res) => {
     }
   };
 
-  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?name=yuz&api_key=C2KZNXSHOPILAEPYOVH6';
+  const url = `https://api.brawlhalla.com/rankings/1v1/brz/1?name=${playerName}&api_key=C2KZNXSHOPILAEPYOVH6`;
 
   https.get(url, options, (apiRes) => {
     let data = '';
