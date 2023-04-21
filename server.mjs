b3219639cc844beaac9afa2b95ffaab2f5891113
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-21 14:37:26 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -9,9 +9,8 @@ createServer((req, res) => {
     }
   };
 
-  const playerName = document.getElementById('player-name').value;
-  const url = `https://api.brawlhalla.com/rankings/1v1/brz/1?name=${playerName}&api_key=C2KZNXSHOPILAEPYOVH6`;
-  
+  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?api_key=C2KZNXSHOPILAEPYOVH6';
+
   https.get(url, options, (apiRes) => {
     let data = '';
 
