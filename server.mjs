72cfc7f7a589716fa5cbe058c8f2ce0b24f842be
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-04-21 15:53:23 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,13 +1,9 @@
 import { createServer } from 'http';
 import https from 'https';
-import url from 'url';
 
 createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const pathName = parsedUrl.pathname;
-  
-  const playerName = pathName.match(/^\/(.+)/)[1];
-  
+  const playerName = req.url.split('/')[1];
+
   const options = {
     method: 'GET',
     headers: {
@@ -15,21 +11,46 @@ createServer((req, res) => {
     }
   };
 
-  const url = 'https://api.brawlhalla.com/rankings/1v1/brz/1?name=${playerName}&api_key=C2KZNXSHOPILAEPYOVH6';
+  let currentPage = 1;
+  let foundPlayer = false;
 
-  https.get(url, options, (apiRes) => {
-    let data = '';
+  const fetchPlayerData = () => {
+    const url = `https://api.brawlhalla.com/rankings/1v1/brz/${currentPage}?api_key=C2KZNXSHOPILAEPYOVH6`;
 
-    apiRes.on('data', (chunk) => {
-      data += chunk;
+    https.get(url, options, (apiRes) => {
+      let data = '';
+
+      apiRes.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      apiRes.on('end', () => {
+        const jsonData = JSON.parse(data);
+
+        const playerData = jsonData.filter(row => row.name.toLowerCase() === playerName.toLowerCase());
+
+        if (playerData.length > 0) {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify(playerData[0]));
+          res.end();
+          foundPlayer = true;
+        } else if (jsonData.length === 100 && !foundPlayer) {
+          currentPage++;
+          fetchPlayerData();
+        } else {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+          res.writeHead(404, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify({ error: 'Jogador não encontrado' }));
+          res.end();
+        }
+      });
+    }).on('error', (err) => {
+      console.error(err);
     });
+  };
 
-    apiRes.on('end', () => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(data);
-      res.end();
-    });
-  });
+  fetchPlayerData();
 }).listen(process.env.PORT);
